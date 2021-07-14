425d6d86879c88275603b9752b6e9172d61af293
Sujit27 <sujit271290@gmail.com>
2021-07-14 20:25:08 +0530

    added flow to get srt for audio file of any length using server side segmentation

--- main.py
+++ main.py
@@ -2,22 +2,13 @@ import grpc
 from stub.speech_recognition_open_api_pb2_grpc import SpeechRecognizerStub
 from stub.speech_recognition_open_api_pb2 import Language, RecognitionConfig, RecognitionAudio, \
     SpeechRecognitionRequest
-import wave
 from grpc_interceptor import ClientCallDetails, ClientInterceptor
-import uuid
-import pafy
-import time
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
 import os
+import shutil
 import subprocess
-import generate_chunks 
-import youtube_dl
-import requests
+import generate_chunks
+from utilities import *
 import config
-import json
-import time
-from pathlib import Path
 from argparse import ArgumentParser
 
 
@@ -51,146 +42,24 @@ class MetadataClientInterceptor(ClientInterceptor):
 
         return method(request_or_iterator, new_details)
 
-def get_auth_token():
-    try:
-        res  = requests.post(config.LOGIN,json={"userName": config.USER,"password":config.PASS})
-        auth_token = res.json()['data']['token']
-        print(" Authentication successful \n")
-        return auth_token
-        
-    except Exception as e:
-        print('Error in authentication {}'.format(e),exc_info=True )
-        return None
-
-def get_model_id(token, src_lang_code,tgt_lang_code):
-    headers = {
-    'auth-token': token
-            }
-    response = requests.get(config.FETCH_MODEL, headers=headers)
-    response = json.loads(response.content)['data']
-    for entry in response:
-        if entry['target_language_code'] == tgt_lang_code and\
-            entry['source_language_code'] == src_lang_code and\
-            entry['status'] == 'ACTIVE' and\
-            'AAI4B' in entry['description']:
-#             print(json.dumps(entry, indent =2))
-            return entry['model_id']
-
-def get_translation( token, model_id, src_lang_code, tgt_lang_code, text):
-    # token = get_auth_token()
-    headers = {
-        'auth-token': token,
-        'Content-Type': 'application/json',
-    }
-    # model_id = get_model_id(token, src_lang_code, tgt_lang_code)
-    data = { "model_id":model_id, "source_language_code":src_lang_code, "target_language_code":tgt_lang_code, "src_list":[ { "src":text } ] }
-
-    response = requests.post(config.TRANSLATE_SEN, headers=headers, data=json.dumps(data))
-    return json.loads(response.content)['data'][0]['tgt']
-
-def read_audio():
-    with wave.open('saved_audio_new.wav', 'rb') as f:
-        return f.readframes(f.getnframes())
-
-
-def transcribe_audio_bytes(stub):
-    language = "hi"
-    audio_bytes = read_audio()
-    lang = Language(value=language, name='Hindi')
-    config = RecognitionConfig(language=lang, audioFormat='MP3', transcriptionFormat='TRANSCRIPT',
-                               enableAutomaticPunctuation=1)
-    audio = RecognitionAudio(audioContent=audio_bytes)
-    request = SpeechRecognitionRequest(audio=audio, config=config)
-
-    # creds = grpc.metadata_call_credentials(
-    #     metadata_plugin=GrpcAuth('access_key')
-    # )
-    try:
-        response = stub.recognize(request)
-
-        print(response.transcript)
-    except grpc.RpcError as e:
-        e.details()
-        status_code = e.code()
-        print(status_code.name)
-        print(status_code.value)
-
-
-def transcribe_audio_url(stub):
-    language = "hi"
-    url = "https://codmento.com/ekstep/test/changed.wav"
-    lang = Language(value=language, name='Hindi')
-    config = RecognitionConfig(language=lang, audioFormat='WAV', enableAutomaticPunctuation=True)
-    audio = RecognitionAudio(audioUri=url)
-    request = SpeechRecognitionRequest(audio=audio, config=config)
-
-    response = stub.recognize(request)
-
-    print(response.transcript)
-
-
-def get_srt_audio_bytes(stub):
-    language = "hi"
-    audio_bytes = read_audio()
-    lang = Language(value=language, name='Hindi')
-    config = RecognitionConfig(language=lang, audioFormat='WAV', transcriptionFormat='SRT',
-                               enableInverseTextNormalization=False)
-    audio = RecognitionAudio(audioContent=audio_bytes)
-    request = SpeechRecognitionRequest(audio=audio, config=config)
-
-    # creds = grpc.metadata_call_credentials(
-    #     metadata_plugin=GrpcAuth('access_key')
-    # )
-    response = stub.recognize(request)
-
-    print(response.srt)
-
-
-
-
-def get_srt_audio_url(stub):
-    language = "hi"
-    url = "https://codmento.com/ekstep/test/changed.wav"
-    lang = Language(value=language, name='Hindi')
-    config = RecognitionConfig(language=lang, audioFormat='WAV', transcriptionFormat='SRT')
-    audio = RecognitionAudio(audioUri=url)
-    request = SpeechRecognitionRequest(audio=audio, config=config)
-
-    response = stub.recognize(request)
-
-    print(response.srt)
-
-
-
-
-def read_given_audio(single_chunk):
-    with wave.open(single_chunk, 'rb') as f:
-        return f.readframes(f.getnframes())
-
-def convert(seconds):
-    try:
-        milli=str(seconds).split('.')[-1][:2]
-    except:
-        milli='00'
-    return time.strftime(f"%H:%M:%S,{milli}", time.gmtime(seconds))
-
 
 def get_text_from_wavfile_any_length(stub,audio_file,lang, translation):
+    '''
+    Given an audio file, segments it and transcribes each segment
+    '''
 
-    langfull={"hi":"Hindi","mr":"Marathi","en":"English","gu":"Gujarati"}
-
-    output_file_path,start_time_stamp,end_time_stamp=generate_chunks.split_and_store(audio_file)
+    output_file_path,start_time_stamp,end_time_stamp = generate_chunks.split_and_store(audio_file)
     result = ''
     token = get_auth_token()
     model_id = get_model_id(token, lang, "en")
     for j in range(len(start_time_stamp)):
         single_chunk=os.path.join(output_file_path ,f'chunk{j}.wav')
         audio_bytes = read_given_audio(single_chunk)
-        lang1 = Language(value=lang, name=langfull[lang])
-        config = RecognitionConfig(language=lang1, audioFormat='MP3', transcriptionFormat='TRANSCRIPT',
+        lang1 = Language(value=lang, name=config.language_code_dict[lang])
+        recog_config = RecognitionConfig(language=lang1, audioFormat='MP3', transcriptionFormat='TRANSCRIPT',
                                 enableAutomaticPunctuation=1)
         audio = RecognitionAudio(audioContent=audio_bytes)
-        request = SpeechRecognitionRequest(audio=audio, config=config)
+        request = SpeechRecognitionRequest(audio=audio, config=recog_config)
 
         # creds = grpc.metadata_call_credentials(
         #     metadata_plugin=GrpcAuth('access_key')
@@ -226,32 +95,39 @@ def get_text_from_wavfile_any_length(stub,audio_file,lang, translation):
         text_file.write(result)
 
 
-def download_youtubeaudio(url, output_file='saved_audio.wav'):
-    try:
-        filepath = str(uuid.uuid4())+".wav"
-        ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': filepath,
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'wav',
-            'preferredquality': '192',
-            
-        }]
-        }
+def gen_srt_limited_duration(stub,audio_file,language,output_file_path):
+    '''
+    Given an audio file, generates srt for the first 5 min
+    '''
+    audio_bytes = read_given_audio(audio_file)
+    lang = Language(value=language, name=config.language_code_dict[language])
+    recog_config = RecognitionConfig(language=lang, audioFormat='WAV', transcriptionFormat='SRT',
+                               enableInverseTextNormalization=False)
+    audio = RecognitionAudio(audioContent=audio_bytes)
+    request = SpeechRecognitionRequest(audio=audio, config=recog_config)
+    response = stub.recognize(request)
 
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-        subprocess.call(["ffmpeg -i {} -ar {} -ac {} -bits_per_raw_sample {} -vn {}".format(filepath, 16000, 1, 16, output_file)], shell=True)
-        os.remove(filepath)
-        return output_file
-    except Exception as e:
-        print(e)
+    with open(output_file_path, "w") as text_file:
+        text_file.write(response.srt)
 
+def gen_srt_full(stub,audio_file,language, translate_to_en):
+    '''
+    Given an audio file, generates srt 
+    '''
+    output_dir = 'chunks'
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
+    chunk_files = chunk_audio(audio_file,output_dir)
+    output_files = []
+    for index,chunk in enumerate(chunk_files):
+        output_file_path = os.path.join(output_dir,"subtitle{0}.srt".format(index))
+        print("Generating subtitle output for chunk {}".format(index))
+        gen_srt_limited_duration(stub,chunk,language, output_file_path)
+        output_files.append(output_file_path)
+    merge_srt_files(output_files)
 
 if __name__ == '__main__':
-
-
     parser = ArgumentParser()
 
     parser.add_argument("--url",help="youtub video  url",type=str,required=True)
@@ -259,20 +135,15 @@ if __name__ == '__main__':
     parser.add_argument("--trans_eng",help=" eng Translate ",type=str,)
     args = parser.parse_args()
 
-    translate_eng=False
+    translate_to_en=False
     try:
         trans_eng=(args.trans_eng).lower()
         if (trans_eng)=='true' or  (trans_eng)=='yes':
-            translate_eng=True
+            translate_to_en=True
     except:
         pass
-    print(translate_eng)
 
-
-
-
-
-    url =args.url 
+    url = args.url 
     subprocess.call(['youtube-dl {}'.format(url)], shell=True)
     audio_file = download_youtubeaudio(url)
 
@@ -281,8 +152,5 @@ if __name__ == '__main__':
     with grpc.insecure_channel('52.12.126.83:50051') as channel:
         channel = grpc.intercept_channel(channel, *interceptors)
         stub = SpeechRecognizerStub(channel)
-        # transcribe_audio_url(stub)
-        # transcribe_audio_bytes(stub)
-        # get_srt_audio_url(stub)
-        # get_srt_audio_bytes(stub)
-        get_text_from_wavfile_any_length(stub,audio_file,lang=args.lang_code, translation=translate_eng)
+        # get_text_from_wavfile_any_length(stub,audio_file,lang=args.lang_code, translation=translate_to_en)
+        gen_srt_full(stub,audio_file,args.lang_code, translate_to_en)
---
+++ utilities.py
@@ -0,0 +1,121 @@
+import wave
+import uuid
+import time
+import os
+import subprocess
+import youtube_dl
+import requests
+import config
+import json
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+import pysrt
+
+def read_given_audio(audio_file):
+    with wave.open(audio_file, 'rb') as f:
+        return f.readframes(f.getnframes())
+
+def convert(seconds):
+    try:
+        milli=str(seconds).split('.')[-1][:2]
+    except:
+        milli='00'
+    return time.strftime(f"%H:%M:%S,{milli}", time.gmtime(seconds))
+
+def download_youtubeaudio(url, output_file='saved_audio.wav'):
+    try:
+        filepath = str(uuid.uuid4())+".wav"
+        ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': filepath,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav',
+            'preferredquality': '192',
+            
+        }]
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        subprocess.call(["ffmpeg -y -i {} -ar {} -ac {} -bits_per_raw_sample {} -vn {}".format(filepath, 16000, 1, 16, output_file)], shell=True)
+        os.remove(filepath)
+        return output_file
+    except Exception as e:
+        print(e)
+
+def chunk_audio(wav_file,output_dir):
+    '''
+    creates 5 min chunks of the given wav_file and saves them
+    '''
+    audio = AudioSegment.from_file(wav_file , "wav") 
+    chunk_length_ms = 300000
+    chunks = make_chunks(audio, chunk_length_ms)
+    chunk_paths = []
+    for i, chunk in enumerate(chunks):
+        chunk_path = os.path.join(output_dir,"chunk{0}.wav".format(i))
+        chunk.export(chunk_path, format="wav")
+        chunk_paths.append(chunk_path)
+
+    return chunk_paths
+    
+def merge_srt_files(srt_files,final_file="subtitle.srt"):
+    subs = pysrt.open(srt_files[0])
+    hours = subs[-1].end.hours
+    minutes = subs[-1].end.minutes
+    seconds = subs[-1].end.seconds
+    milliseconds = subs[-1].end.milliseconds
+    for i in range(1,len(srt_files)):
+        subs = pysrt.open(srt_files[i])
+        subs.shift(hours=hours)
+        subs.shift(minutes=minutes)
+        subs.shift(seconds=seconds)
+        subs.shift(milliseconds=milliseconds)
+        subs.save(srt_files[i], encoding='utf-8')
+
+        hours = subs[-1].end.hours
+        minutes = subs[-1].end.minutes
+        seconds = subs[-1].end.seconds
+        milliseconds = subs[-1].end.milliseconds
+
+    with open(final_file,"wb") as output_file:
+        for f in srt_files:
+            with open(f, "rb") as infile:
+                output_file.write(infile.read())
+
+def get_auth_token():
+    try:
+        res  = requests.post(config.LOGIN,json={"userName": config.USER,"password":config.PASS})
+        auth_token = res.json()['data']['token']
+        print(" Authentication successful \n")
+        return auth_token
+        
+    except Exception as e:
+        print('Error in authentication {}'.format(e),exc_info=True )
+        return None
+
+def get_model_id(token, src_lang_code,tgt_lang_code):
+    headers = {
+    'auth-token': token
+            }
+    response = requests.get(config.FETCH_MODEL, headers=headers)
+    response = json.loads(response.content)['data']
+    for entry in response:
+        if entry['target_language_code'] == tgt_lang_code and\
+            entry['source_language_code'] == src_lang_code and\
+            entry['status'] == 'ACTIVE' and\
+            'AAI4B' in entry['description']:
+#             print(json.dumps(entry, indent =2))
+            return entry['model_id']
+
+def get_translation( token, model_id, src_lang_code, tgt_lang_code, text):
+    # token = get_auth_token()
+    headers = {
+        'auth-token': token,
+        'Content-Type': 'application/json',
+    }
+    # model_id = get_model_id(token, src_lang_code, tgt_lang_code)
+    data = { "model_id":model_id, "source_language_code":src_lang_code, "target_language_code":tgt_lang_code, "src_list":[ { "src":text } ] }
+
+    response = requests.post(config.TRANSLATE_SEN, headers=headers, data=json.dumps(data))
+    return json.loads(response.content)['data'][0]['tgt']

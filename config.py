f713c15e3f9682c6900b398c9b2fd4d8d3ddaaa1
Aswin Pradeep <aswinpradeep1@gmail.com>
2021-07-18 23:04:36 +0530

    Adding extra language support

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ USER="aswin.pradeep@tarento.com"
 PASS="@Password1"
 FETCH_MODEL = 'https://users-auth.anuvaad.org/nmt-inference/v2/fetch-models'
 TRANSLATE_SEN = 'https://users-auth.anuvaad.org/aai4b-nmt-inference/v1/translate'
-language_code_dict = {"hi":"Hindi","mr":"Marathi","en":"English","gu":"Gujarati"}
+language_code_dict = {"hi":"Hindi","mr":"Marathi","en":"English","gu":"Gujarati","te":"Telugu","or":"Odia","ta":"Tamil","kn":"Kannada",}

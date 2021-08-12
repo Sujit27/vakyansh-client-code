860efcb6ecae1fb9f157ddb440665cbf936189b6
Aswin Pradeep <aswinpradeep1@gmail.com>
2021-08-12 13:47:25 +0530

    Revert CORS

--- flask_server.py
+++ flask_server.py
@@ -4,9 +4,7 @@ from flask import send_file
 import main
 
 app = Flask(__name__)
-CORS(app, resources={r"*": {"origins": "*"}})
-
-
+CORS(app)
 
 @app.route('/generate_srt',methods=['POST'])
 def generate_srt():

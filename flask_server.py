50da0348bcde84515b8b1626e64a5bafc1190307
Aswin Pradeep <aswinpradeep1@gmail.com>
2021-08-12 14:40:55 +0530

    CORS-4

--- flask_server.py
+++ flask_server.py
@@ -4,7 +4,7 @@ from flask import send_file
 import main
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["*","https://ban-sc.idc.tarento.com"]}})
+CORS(app, resources={r"*": {"origins": ["*","https://ban-sc.idc.tarento.com"]}})
 
 @app.route('/generate_srt',methods=['POST'])
 def generate_srt():

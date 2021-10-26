497285fb395f09d6a39e3cf8dd402f75b7ff0e69
Aswin Pradeep <aswinpradeep1@gmail.com>
2021-10-26 20:44:52 +0530

    change upload dir

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ TRANSLATE_SEN = 'https://users-auth.anuvaad.org/aai4b-nmt-inference/v1/translate
 
 SUBTITLE_DIR = "/home/ec2-user/vakyansh-client-realtime-v2/vakyansh-client-code/subtitles/"
 SPEAKER_DIARIZATION_DIR = "/home/ec2-user/vakyansh-client-realtime-v2/vakyansh-client-code/speaker_diarization/"
-UPLOAD_DIR = "/home/ec2-user/vakyansh-client-realtime-v2/vakyansh-client-code/uploads/"
+UPLOAD_DIR = "uploads/"
 
 language_code_dict = {"hi":"Hindi","mr":"Marathi","en":"English","gu":"Gujarati","te":"Telugu","ta":"Tamil","or":"Odia","kn":"Kannada","bn":"Bengali","ml":"Malayalam"}
 
